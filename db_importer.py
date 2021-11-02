ddace7b11607e59085c646b0c94e50000c0a38b8
Pedro Baptista <ze.pedro3622@outlook.pt>
2021-11-02 19:56:59 +0000

    more imports

--- db_importer.py
+++ db_importer.py
@@ -53,9 +53,41 @@ def parse_district(data):
     ]
     return [parse_row(types, row) for row in data]
 
+def parse_account(data):
+    types = [
+        parse_int,
+        parse_int,
+        parse_string,
+        parse_string
+    ]
+    return [parse_row(types, row) for row in data]
+
+def parse_loan(data):
+    types = [
+        parse_int,
+        parse_int,
+        parse_string,
+        parse_int,
+        parse_int,
+        parse_int,
+        parse_int
+    ]
+    return [parse_row(types, row) for row in data]
 
 with open("clean-data/district.csv", newline="") as district:
     reader = csv.reader(district)
     data = [rows for rows in reader]
     parsed_data = parse_district(data[1:])
-    print(parsed_data[0])
+    # print(parsed_data[0])
+
+with open("clean-data/account.csv", newline="") as account:
+    reader = csv.reader(account)
+    data = [rows for rows in reader]
+    parsed_data = parse_account(data[1:])
+    # print(parsed_data)
+
+with open("clean-data/loan_"+ mode +".csv", newline="") as loan:
+    reader = csv.reader(loan)
+    data = [rows for rows in reader]
+    parsed_data = parse_loan(data[1:])
+    print(parsed_data)
