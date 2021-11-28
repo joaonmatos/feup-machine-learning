2697c2217a3255f1d5e857e588ebaaf4564baea0
Miguel Simões <miguelsimoes20000720@gmail.com>
2021-11-28 18:08:22 +0000

    Absolute frequency tables completed

--- statistics/statistical_methods.py
+++ statistics/statistical_methods.py
@@ -5,6 +5,9 @@ import pandas as pd
 client = pd.read_csv("../clean-data/client.csv")
 account = pd.read_csv("../clean-data/account.csv")
 disposition = pd.read_csv("../clean-data/disposition.csv")
+card = pd.read_csv("../clean-data/card_train.csv")
+loan = pd.read_csv("../clean-data/loan_train.csv")
+transaction = pd.read_csv("../clean-data/transaction_train.csv")
 
 # # %% Gender per Client
 gender_stats = pd.crosstab(index=client['gender'], columns='count')
@@ -23,5 +26,41 @@ frequency_account_stats = pd.crosstab(index=account['frequency'], columns='count
 frequency_account_stats.to_csv("account_frequency.csv")
 
 # %% User type per Disposition
+
 type_disposition_stats = pd.crosstab(index=disposition['type'], columns='count')
 type_disposition_stats.to_csv("type_disposition.csv")
+
+# %% Type per Card (Train)
+
+type_card_stats = pd.crosstab(index=card['type'], columns='count')
+type_card_stats.to_csv("type_card.csv")
+
+# %% Duration per Loan (Train)
+
+duration_loan_stats = pd.crosstab(index=loan['duration'], columns='count')
+duration_loan_stats.to_csv("duration_loan.csv")
+
+# %% Status per Loan (Train)
+
+status_loan_stats = pd.crosstab(index=loan['status'], columns='count')
+status_loan_stats.to_csv("status_loan.csv")
+
+# %% Type per Transaction (Train)
+
+type_transaction_stats = pd.crosstab(index=transaction['type'], columns='count')
+type_transaction_stats.to_csv("type_transaction.csv")
+
+# %% Operation per Transaction (Train)
+
+operation_transaction_stats = pd.crosstab(index=transaction['operation'], columns='count')
+operation_transaction_stats.to_csv("operation_transaction.csv")
+
+# %% K_symbol per Transaction (Train)
+
+k_symbol_transaction_stats = pd.crosstab(index=transaction['k_symbol'], columns='count')
+k_symbol_transaction_stats.to_csv("k_symbol_transaction.csv")
+
+# %% Bank per Transaction (Train)
+
+bank_transaction_stats = pd.crosstab(index=transaction['bank'], columns='count')
+bank_transaction_stats.to_csv("bank_transaction.csv")
